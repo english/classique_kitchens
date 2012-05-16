f33fd5505d695e24d64359c23d6f9c67b9943ba5
Jamie English <jamienglish@gmail.com>
2012-05-16 22:23:00 +0100

    Completed passing contact form feature.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -7,7 +7,7 @@ class ContactController < ApplicationController
     @message = Message.new(params[:message])
     if @message.valid?
       Notifier.new_message(@message).deliver
-      redirect_to root_url, flash: { success: "Message sent successfully. Thank you." }
+      redirect_to root_url, flash: { notice: "Message sent successfully. Thank you." }
     else
       render action: 'new'
     end
--- features/step_definitions/contact_steps.rb
+++ features/step_definitions/contact_steps.rb
@@ -1,19 +1,21 @@
 Given /^I am on the contact page$/ do
-  visit '/'
+  visit '/contact'
 end
 
 When /^I fill in "(.*?)" with "(.*?)"$/ do |field, content|
-  within 'form.contact' do
+  within 'form.contact-form' do
     fill_in field, with: content
   end
 end
 
 When /^I press "(.*?)"$/ do |button|
-  within form.contact do
+  within 'form.contact-form' do
     click_on button
   end
 end
 
 Then /^I should see a success message$/ do
-  page.should have_content('success')
+  within '.content' do
+    page.should have_content('success')
+  end
 end
