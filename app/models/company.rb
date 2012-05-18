7bbc72044c34f0a680d3e7274c73502c86ffb35b
Jamie English <jamienglish@gmail.com>
2012-05-18 21:05:11 +0100

    Added some more copy to front page.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module Company
   extend self
 
@@ -11,13 +13,18 @@ module Company
 
   def blurb
     <<BLURB
-Kitchens from Classique Kitchens are a reflection of human individuality,
+Kitchens from __Classique Kitchens__ are a reflection of human individuality,
 a sign of personal style. Whatever the kitchen means to you, routine,
 relaxation, hobby or profession.
 
 We will work with you to create a totally unique kitchen acquired to your
 personal taste. We perceive each individual project as an invitation to push
 our creative design skills to the limit, above all with new thinking.
+
+Our Kitchens are carefully selected from high quality branded European
+suppliers, ensuring you the very latest in cutting edge design and innovation.
+Our portfolio of over 50 ranges include high gloss lacquers, carbon acrylic
+through to our bespoke handmade furniture.
 BLURB
   end
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,5 +3,5 @@ require_relative '../../app/models/company'
 describe Company do
   its(:phone_number) { should == "01228 525676" }
   its(:address) { should == "Unit 2, Port Road Workshops, Carlisle, Cumbria, CA2 7AJ" }
-  its(:blurb) { should =~ /^Kitchens from Classique Kitchens are a reflection of/ }
+  its(:blurb) { should =~ /Classique Kitchens/ }
 end
