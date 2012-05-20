b3383b95e3f1487dd9c64122ec0a5d4c8d779b3d
Jamie English <jamienglish@gmail.com>
2012-05-20 16:26:03 +0100

    Added another paragraph to stormer kitchens page

--- app/controllers/stormer_kitchens_controller.rb
+++ app/controllers/stormer_kitchens_controller.rb
@@ -6,10 +6,12 @@ class StormerKitchensController < ApplicationController
     @description = StormerKitchen.description
     @active_nav_item = 'störmer küchens'
     @features = Feature.all
+    @final_message = StormerKitchen.final_message
   end
-  attr_reader :kitchens, :type, :description, :features
+  attr_reader :kitchens, :type, :description, :features, :final_message
   helper_method :kitchens
   helper_method :type
   helper_method :description
   helper_method :features
+  helper_method :final_message
 end
--- app/models/stormer_kitchen.rb
+++ app/models/stormer_kitchen.rb
@@ -17,4 +17,15 @@ Modern and timeless kitchens...
 offer many possibilities of design arranged in a defined space. Superior quality and the latest design trends are accentuated by innovative solutions and the highest quality materials. Our desire is to design a kitchen which is perfect both aesthetically and functionally, with attention paid to the smallest of details.
 DESC
   end
+
+  def self.final_message
+    RDiscount.new(<<MSG).to_html
+störmer; Embracing the trend for natural materials, störmer recently launched a new collection of wood laminates and veneers. This enhances the ranges available through ST130, the flexible grid design system developed by störmer.
+
+The Floating range was launched at the same time, which, along with the Linea and Half and Full grid ranges, accommodates even the most demanding design specifications. störmer's "Just in time" service available on the lower price groups executes a 12 day turnaround from point of order.
+
+This, along with 8mm backs for all units, competitive pricing and many other aspects ensures that störmer is a viable market offering, encompassing a broad range of affordable styles and designs.
+  end
+MSG
+  end
 end
