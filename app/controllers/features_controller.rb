969da93c7dc619a6ad705de155f8401f572a5e88
Jamie English <jamienglish@gmail.com>
2012-05-20 11:38:03 +0100

    Setup simple controller and view for feature

---
+++ app/controllers/features_controller.rb
@@ -0,0 +1,40 @@
+class FeaturesController < ApplicationController
+  def new
+    @feature = Feature.new
+  end
+
+  def show
+    @feature = Feature.find(params[:id])
+  end
+
+  def edit
+    @feature = Feature.find(params[:id])
+  end
+
+  def create
+    @feature = Feature.new(params[:feature])
+
+    if @feature.save
+      redirect_to @feature, notice: 'Feature was successfully created.'
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @feature = Feature.find(params[:id])
+
+    if @feature.update_attributes(params[:feature])
+      redirect_to @feature, notice: 'Feature was successfully updated.'
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @feature = Feature.find(params[:id])
+    @feature.destroy
+
+    redirect_to features_url
+  end
+end
