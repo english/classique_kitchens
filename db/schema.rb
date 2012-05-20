98b6b37e3e4e856084fe423c3e315a25ab0d390a
Jamie English <jamienglish@gmail.com>
2012-05-20 11:26:59 +0100

    (re)added image columns to feature table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120520081350) do
+ActiveRecord::Schema.define(:version => 20120520102451) do
 
   create_table "features", :force => true do |t|
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "kitchens", :force => true do |t|
