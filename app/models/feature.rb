6da4847adc780630f3fc2526dc5cf9e7ba48f548
Jamie English <jamienglish@gmail.com>
2012-05-20 09:28:06 +0100

    Passed specs for feature

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,3 +1,9 @@
 class Feature < ActiveRecord::Base
   attr_accessible :content
+  has_attached_file :image, styles: { main: '200x230#' }
+  validates_attachment_presence :image
+
+  def content_html
+    RDiscount.new(self.content).to_html.strip
+  end
 end
