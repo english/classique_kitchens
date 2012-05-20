5b4e9edbad6d2e12a8a42206525b6668291a0449
Jamie English <jamienglish@gmail.com>
2012-05-20 11:54:51 +0100

    List features in /stomer_kitchens

--- app/controllers/stormer_kitchens_controller.rb
+++ app/controllers/stormer_kitchens_controller.rb
@@ -5,9 +5,11 @@ class StormerKitchensController < ApplicationController
     @type = StormerKitchen.type.pluralize
     @description = StormerKitchen.description
     @active_nav_item = 'störmer küchens'
+    @features = Feature.all
   end
-  attr_reader :kitchens, :type, :description
+  attr_reader :kitchens, :type, :description, :features
   helper_method :kitchens
   helper_method :type
   helper_method :description
+  helper_method :features
 end
