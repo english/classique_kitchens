eddb6022159b355340f7b3473cb9dca2db88608b
Jamie English <jamienglish@gmail.com>
2012-05-20 10:17:31 +0100

    Added rest routes to feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 ClassiqueKitchens::Application.routes.draw do
-  get "stormer_kitchens" => 'stormer_kitchens#index'
-  get "hand_made_furniture" => 'hand_made_furniture#index'
-
   root to: 'page#home'
+
   resources :kitchens
-  get "contact" => 'contact#new'
-  put "contact" => 'contact#create'
+  resources :features
+  resources :contact
+
+  get 'stormer_kitchens'    => 'stormer_kitchens#index'
+  get 'hand_made_furniture' => 'hand_made_furniture#index'
 end
