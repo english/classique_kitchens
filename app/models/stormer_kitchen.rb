1f2ecfbece94428e3263dc3dec0c665afe82a292
Jamie English <jamienglish@gmail.com>
2012-05-23 19:13:45 +0100

    Tidied up long strings

--- app/models/stormer_kitchen.rb
+++ app/models/stormer_kitchen.rb
@@ -14,18 +14,31 @@ class StormerKitchen < Kitchen
     RDiscount.new(<<DESC).to_html
 Modern and timeless kitchens...
 
-offer many possibilities of design arranged in a defined space. Superior quality and the latest design trends are accentuated by innovative solutions and the highest quality materials. Our desire is to design a kitchen which is perfect both aesthetically and functionally, with attention paid to the smallest of details.
+offer many possibilities of design arranged in a defined space. Superior
+quality and the latest design trends are accentuated by innovative solutions
+and the highest quality materials. Our desire is to design a kitchen which is
+perfect both aesthetically and functionally, with attention paid to the
+smallest of details.
+
+Read the [2012 störmer
+jounal](http://www.stoermer-kuechen.de/journal/index-gb.shtml#journal).
 DESC
   end
 
   def self.final_message
     RDiscount.new(<<MSG).to_html
-störmer; Embracing the trend for natural materials, störmer recently launched a new collection of wood laminates and veneers. This enhances the ranges available through ST130, the flexible grid design system developed by störmer.
+störmer; Embracing the trend for natural materials, störmer recently launched
+a new collection of wood laminates and veneers. This enhances the ranges
+available through ST130, the flexible grid design system developed by störmer.
 
-The Floating range was launched at the same time, which, along with the Linea and Half and Full grid ranges, accommodates even the most demanding design specifications. störmer's "Just in time" service available on the lower price groups executes a 12 day turnaround from point of order.
+The Floating range was launched at the same time, which, along with the Linea
+and Half and Full grid ranges, accommodates even the most demanding design
+specifications. störmer's "Just in time" service available on the lower price
+groups executes a 12 day turnaround from point of order.
 
-This, along with 8mm backs for all units, competitive pricing and many other aspects ensures that störmer is a viable market offering, encompassing a broad range of affordable styles and designs.
-  end
+This, along with 8mm backs for all units, competitive pricing and many other
+aspects ensures that störmer is a viable market offering, encompassing a broad
+range of affordable styles and designs.
 MSG
   end
 end
