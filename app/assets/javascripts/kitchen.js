5c94180ecb7638419e5b521805a0dc19942f8319
Jamie English <jamienglish@gmail.com>
2012-11-07 22:15:08 +0000

    Only run slideshow for stormer kitchens page

--- app/assets/javascripts/kitchen.js
+++ app/assets/javascripts/kitchen.js
@@ -1,8 +1,8 @@
 $(function() {
-  $('#slideshow .kitchen:gt(0)').hide();
+  $('.stormer #slideshow .kitchen:gt(0)').hide();
 
   setInterval(function() {
-    $('#slideshow .kitchen:first-child').fadeOut(1000)
+    $('.stormer #slideshow .kitchen:first-child').fadeOut(1000)
       .next('.kitchen').fadeIn(1000)
       .end().appendTo('#slideshow');
   }, 5000);
